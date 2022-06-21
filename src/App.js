4e337472c41a70110267293aa9056c986a1cf847
SazidAhmed <sazidahmed.official@gmail.com>
2022-06-21 23:55:57 +0600

    icons added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,92 @@
-import { Card, Container, Button, Row, Col } from 'react-bootstrap';
+
+import { useState } from 'react';
+import { FaEye } from 'react-icons/fa';
+import { Card, Container, Button, Row, Col, Modal } from 'react-bootstrap';
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <Container>
-  <Row>
-    <Col>1 of 2</Col>
-    <Col>2 of 2</Col>
-  </Row>
-  <Row>
-    <Col>1 of 3</Col>
-    <Col>2 of 3</Col>
-    <Col>3 of 3</Col>
-  </Row>
-</Container>
+      <Row>
+        <Col sm={3}>
+          <Card style={{ width: '18rem' }} onClick={handleShow}>
+            <Card.Img variant="top" src="img/default.jpg" />
+            <Card.Body>
+              <Card.Title>Pokemon</Card.Title>
+              <Card.Text>
+               Click on More to view in details
+              </Card.Text>
+              
+              <Button variant="primary" onClick={handleShow}><FaEye /></Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col sm={3}>
+          <Card style={{ width: '18rem' }} onClick={handleShow}>
+            <Card.Img variant="top" src="img/default.jpg" />
+            <Card.Body>
+              <Card.Title>Pokemon</Card.Title>
+              <Card.Text>
+               Click on More to view in details
+              </Card.Text>
+              
+              <Button variant="primary" onClick={handleShow}><FaEye /></Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col sm={3}>
+          <Card style={{ width: '18rem' }} onClick={handleShow}>
+            <Card.Img variant="top" src="img/default.jpg" />
+            <Card.Body>
+              <Card.Title>Pokemon</Card.Title>
+              <Card.Text>
+               Click on More to view in details
+              </Card.Text>
+              
+              <Button variant="primary" onClick={handleShow}><FaEye /></Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col sm={3}>
+          <Card style={{ width: '18rem' }} onClick={handleShow}>
+            <Card.Img variant="top" src="img/default.jpg" />
+            <Card.Body>
+              <Card.Title>Pokemon</Card.Title>
+              <Card.Text>
+               Click on More to view in details
+              </Card.Text>
+              
+              <Button variant="primary" onClick={handleShow}><FaEye /></Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+      {/* modal */}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Pokemon Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="img/default.jpg" />
+          </Card>
+          <h4>Pokemon</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit culpa</p>
+          <h2>Description</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi voluptate laudantium nam recusandae, eligendi ratione voluptatibus quos inventore soluta magnam! Repellendus ipsum natus ducimus sint, culpa labore possimus quibusdam numquam.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Container>
   );
 }
 
